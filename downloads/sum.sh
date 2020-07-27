82e75147032d95bcdf1fd88734fe037b0e5b9df0
Markus Iser <markus.iser@kit.edu>
2020-07-27 03:59:12 +0200

    fixed could-track penalty; added nolimits track table

--- downloads/sum.sh
+++ downloads/sum.sh
@@ -1,6 +1,12 @@
 if [ $# -lt 2 ]; then
-	echo "Usage: $0 [results] [total]"
+	echo "Usage: $0 [results] [total] [penalty]"
 	exit 1
+fi
+
+if [ $# -eq 3 ]; then
+	penalty=$3
+else
+	penalty=10000
 fi
 
 file=$1
@@ -12,11 +18,11 @@ for i in $(seq 3 $maxc); do
 	times=$(tail -n+2 $file | cut -d, -f$i | xargs); 
 	nsolved=$(echo $times | sed 's/ FAILED\| TIMEOUT\| INCORRECT\| UNKNOWN//g' | wc -w); 
 	incorrect=$(if [ $(echo $times | grep -v INCORRECT | wc -w) -eq 0 ]; then echo "disqualified"; else echo " "; fi); 
-	sum=$(echo $times | sed 's/FAILED\|TIMEOUT\|INCORRECT\|UNKNOWN/10000/g' | sed 's/ /+/g'); 
+	sum=$(echo $times | sed "s/FAILED\|TIMEOUT\|INCORRECT\|UNKNOWN/$penalty/g" | sed 's/ /+/g'); 
 
 	# par2 fix (sat/unsat tracks, fill 200)
 	count=$(echo $sum | sed 's/+/ /g' | wc -w)
-	for c in $(seq $((count+1)) $total); do sum="$sum+10000"; done
+	for c in $(seq $((count+1)) $total); do sum="$sum+$penalty"; done
 	#echo $sum | sed 's/+/ /g' | wc -w
 
 	par2=$(echo "($sum)/$total" | bc); 
